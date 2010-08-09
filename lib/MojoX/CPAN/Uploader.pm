bc146a34324d2f7249f29a3c4a999be43c42321b
Yaroslav Korshak <ykorshak@gmail.com>
2010-08-10 01:08:35 +0300

    vti forced me to do this :'(

---
+++ lib/MojoX/CPAN/Uploader.pm
@@ -0,0 +1,129 @@
+# Copyright (C) 2010, Yaroslav Korshak.
+ 
+package MojoX::CPAN::Uploader;
+
+use warnings;
+use strict;
+use Carp;
+use File::Basename;
+
+use base 'Mojo::Base';
+
+require Mojo::Client;
+require IO::Socket::SSL;
+
+__PACKAGE__->attr(client => sub { Mojo::Client->new });
+__PACKAGE__->attr([qw/user pass/]);
+__PACKAGE__->attr(url => sub { Mojo::URL->new('https://pause.perl.org/pause/authenquery') });
+__PACKAGE__->attr(defaults => sub {
+{
+      CAN_MULTIPART                     => 1,
+      SUBMIT_pause99_add_uri_httpupload => " Upload this file from my disk ",
+      pause99_add_uri_uri               => "",
+}
+});
+
+our $VERSION = '0.0.2';
+
+sub auth {
+    my $self = shift;
+
+    croak("Basic authorization user name can't contain ':'")
+        if $_[0] =~ /:/;
+
+    $self->user(shift);
+    $self->pass(shift);
+}
+
+sub upload {
+    my ($self, $file, $subdir)  = splice @_, 0, 3 ;
+    my $url = $self->url->clone->userinfo($self->user . ':' . $self->pass);
+
+    croak "Auth info required!" unless $self->user;
+
+    return $self->client->post_form(
+        $url,
+        {   %{$self->defaults},
+            HIDDENNAME                 => $self->user,
+            pause99_add_uri_upload     => basename($file),
+            pause99_add_uri_httpupload => {file => $file},
+            ($subdir ? (pause99_add_uri_subdirtext => $subdir) : ()),
+        },
+        @_
+    );
+}
+
+
+1;
+__END__
+
+=head1 NAME
+
+MojoX::CPAN::Uploader - Mojo way to upload on CPAN
+
+
+=head1 VERSION
+
+This document describes MojoX::CPAN::Uploader version 0.0.2
+
+
+=head1 SYNOPSIS
+
+    use MojoX::CPAN::Uploader;
+
+=for author to fill in:
+    Brief code example(s) here showing commonest usage(s).
+    This section will be as far as many users bother reading
+    so make it as educational and exeplary as possible.
+  
+  
+=head1 DESCRIPTION
+
+=for author to fill in:
+    Write a full description of the module and its features here.
+    Use subsections (=head2, =head3) as appropriate.
+
+
+=head1 INTERFACE 
+
+=for author to fill in:
+    Write a separate section listing the public components of the modules
+    interface. These normally consist of either subroutines that may be
+    exported, or methods that may be called on objects belonging to the
+    classes provided by the module.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=for author to fill in:
+    A full explanation of any configuration system(s) used by the
+    module, including the names and locations of any configuration
+    files, and the meaning of any environment variables or properties
+    that can be set. These descriptions must also include details of any
+    configuration language used.
+  
+MojoX::CPAN::Uploader requires no configuration files or environment variables.
+
+
+=head1 DEPENDENCIES
+
+=for author to fill in:
+    A list of all the other modules that this module relies upon,
+    including any restrictions on versions, and an indication whether
+    the module is part of the standard Perl distribution, part of the
+    module's distribution, or must be installed separately. ]
+
+None.
+
+
+=head1 AUTHOR
+
+Yaroslav Korshak  C<< <ykorshak@gmail.com> >>
+
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2010, Yaroslav Korshak C<< <ykorshak@gmail.com> >>. All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
---
+++ t/01.mojo-cpan-uploader.t
@@ -0,0 +1,80 @@
+use Test::More tests => 19;
+use MIME::Base64;
+use Mojolicious::Lite;
+use Test::Mojo;
+use File::Temp qw/ tempfile /;
+
+plan skip_all => 'working sockets required for this test!'
+  unless Mojo::IOLoop->new->generate_port;
+
+app->log->path(undef);
+app->log->level('error');
+
+BEGIN {
+    use_ok('MojoX::CPAN::Uploader');
+}
+
+any '/' => sub {
+    shift->render_text("OK");
+};
+
+post '/auth' => sub {
+    my $self = shift;
+    my $auth = $self->req->headers->header('Authorization');
+    ok($auth =~ qr/^Basic (.*)/);
+    is($1, MIME::Base64::encode('user:pass', ''));
+
+    is($self->param('HIDDENNAME'),    'user');
+    is($self->param('CAN_MULTIPART'), 1);
+    ok($self->param('pause99_add_uri_upload'));
+    is($self->param('SUBMIT_pause99_add_uri_httpupload'),
+        " Upload this file from my disk ");
+
+    ok(!defined $self->param('pause99_add_uri_subdirtext'));
+
+    if (ok(1)) {
+        $self->render_text("OK");
+    }
+    else {
+        $self->render_text("Fail");
+    }
+};
+
+post '/auth/subdir' => sub {
+    my $self = shift;
+    my $auth = $self->req->headers->header('Authorization');
+    ok($auth =~ qr/^Basic (.*)/);
+    is($1, MIME::Base64::encode('user:pass', ''));
+
+    is($self->param('pause99_add_uri_subdirtext'), 'someDir');
+
+    $self->render_text("OK");
+};
+
+diag("Building transaction with basic auth");
+my $t = Test::Mojo->new;
+
+$t->get_ok('/')->status_is(200);
+
+my $up = MojoX::CPAN::Uploader->new;
+
+isa_ok($up, 'MojoX::CPAN::Uploader');
+
+$up->url($t->tx->req->url->clone->path('/auth'));
+
+$up->client($t->client);
+$up->auth('user', 'pass');
+
+my ($fh, $filename) = tempfile();
+
+my $tx = $up->upload($filename, undef);
+
+isa_ok($tx, 'Mojo::Transaction::HTTP');
+is($tx->res->body, 'OK');
+
+$up->url->path('subdir');
+
+$tx = $up->upload($filename, 'someDir');
+
+isa_ok($tx, 'Mojo::Transaction::HTTP');
+is($tx->res->body, 'OK');
